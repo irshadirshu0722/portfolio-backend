d868a866263fefb8b138af764292bb6f8bef6945
alafzah123 <email.alafzah@gmail.com>
2024-09-02 15:04:09 +0530

    second commi

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
+
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput -clear
 echo " BUILD END"
-=======
-echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput -clear
-echo " BUILD END"
->>>>>>> 66553b3d07239078a1e22370226e9dc3614d4cff
--- manage.py
+++ manage.py
@@ -19,6 +19,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-#!/usr/bin/env python
-if __name__ == '__main__':
-    main()
